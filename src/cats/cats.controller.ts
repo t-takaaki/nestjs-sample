28a6bb75c1ca25bcb7b4d399bdf6a938a149a89b
t-takaaki <hokahoka.oden@gmail.com>
2021-01-14 17:05:58 +0900

    create service & views

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,16 +1,34 @@
-import { Controller, Get, HttpCode, Param } from '@nestjs/common';
+import { Controller, Get, Post, HttpCode, Param, Body, Render } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
   @Get()
-  findAll(): string {
-    return 'This action returns all cats.';
+  async findAll(): Promise<Cat[]> {
+    // return 'This action returns all cats.';
+    return this.catsService.findAll();
   }
 
   @Get('/hogehoge')
   @HttpCode(404)
   notFound(): string {
     return 'There are No Cats In America.'
+  }
+
+  @Get('new')
+  @Render('cats')
+  new() {
+    return { message: 'hogehoge' };
+  }
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    // return 'This action adds a new cat.';
+    this.catsService.create(createCatDto);
   }
 
   @Get(':id')
